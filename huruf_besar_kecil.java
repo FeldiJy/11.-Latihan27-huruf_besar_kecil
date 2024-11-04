e72c7cb6136ef443cfe404a207a5c2e4d8ad6617
FeldiJy <irengbuluken@gmail.com>
2024-11-04 19:19:37 +0700

    Add files via upload

---
+++ huruf_besar_kecil.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class huruf_besar_kecil {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Masukkan Kalimat: ");
+        String input = scanner.nextLine();
+        
+        System.out.println("Masukkan Kalimat: " + input);
+    }
+}
